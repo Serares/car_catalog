eb23c280e69ff7f28589e0c8a5a8e196f7c2eaef
Serares <jianu.rares30@gmail.com>
2019-12-29 16:36:51 +0200

    It a starting point for requests
    Now i have a boilerplate for axios in services folder so It will be  easy to send req

---
+++ src/views/Admin.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <div>Welcome to admin page</div>
+    <ul>
+      <li>
+        <a>Add Car</a>
+      </li>
+      <li>
+        <a>Modify Car</a>
+      </li>
+      <li>
+        <a>Delete Car</a>
+      </li>
+    </ul>
+    <input type="text" v-model="email" placeholder="email" />
+    <input type="button" @click="register" value="Click ME!"/>
+  </div>
+</template>
+
+<script>
+import Authentication from '@/services/AuthenticationService';
+
+export default {
+  data() {
+    return {
+      email: "abc"
+    };
+  },
+  methods:{
+      async register(){
+          const response = await Authentication.register({
+              email: this.email
+          });
+          console.log(response.data);
+      }
+  }
+};
+</script>
