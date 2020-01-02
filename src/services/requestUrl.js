113f21c1ff34c3179ec34d33de6573702f1b2ef6
Serares <jianu.rares30@gmail.com>
2020-01-03 00:59:50 +0200

    checking on live

--- src/services/requestUrl.js
+++ src/services/requestUrl.js
@@ -3,11 +3,12 @@
 /*
 -- base urls for requests
 */
-// const prodURL = "https://car-catalog-server.herokuapp.com";
+const prodURL = "https://car-catalog-server.herokuapp.com";
 const devURL = "http://localhost:5500";
 
 export default {
     reqUrl(){
-        return devURL;
+        devURL;
+        return prodURL;
     }
 }
